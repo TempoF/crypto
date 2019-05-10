905ad6e3e9d7f7df8b3cd2074cf16da06b6d74dd
TempoF <39939852+FerdinandTempori@users.noreply.github.com>
2019-05-10 03:54:49 -0500

    commit

--- Reg/src/Package/AddCandidates.java
+++ Reg/src/Package/AddCandidates.java
@@ -203,7 +203,7 @@ public class AddCandidates extends javax.swing.JFrame {
         } catch (InstantiationException ex) {
             java.util.logging.Logger.getLogger(AddCandidates.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.utilLabel_Nameg.Logger.getLogger(AddCandidates.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddCandidates.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(AddCandidates.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
